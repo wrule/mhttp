bf3f09572d34f6932efa90c9eb0b2acc0a86b8e9
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-29 12:53:51 +0800

    http基本没问题

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
+import * as mockhttp from 'mockttp';
 
-console.log('你好，世界');
+function main() {
+  const server = mockhttp.getLocal();
+  server.on('request', (data) => {
+    console.log(data.url);
+  });
+  server.on('response', (data) => {
+    console.log(data.body.buffer.toString());
+  });
+  server.forAnyRequest().thenPassThrough();
+  server.start(8899);
+}
+
+main();
