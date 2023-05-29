71ead17503f62123624af2bd2706639c5e931f00
jimao <jimao@jimaodeMacBook-Pro.local>
2023-05-29 13:33:37 +0800

    https ok

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import * as mockhttp from 'mockttp';
 
 function main() {
-  const server = mockhttp.getLocal();
+  const server = mockhttp.getLocal({
+    https: {
+      keyPath: './ca/testCA.key',
+      certPath: './ca/testCA.pem',
+    },
+  });
   server.on('request', (data) => {
     console.log(data.url);
   });
   server.on('response', (data) => {
-    console.log(data.body.buffer.toString());
+    // console.log(data.body.buffer.toString());
   });
   server.forAnyRequest().thenPassThrough();
   server.start(8899);
